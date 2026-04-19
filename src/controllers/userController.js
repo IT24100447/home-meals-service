27918f25baa6a053a62c531088275fdf32e51060
IT24100447 <it24100447@my.sliit.lk>
2026-04-19 23:04:38 +0530

    Fixed Bugs and Connectd to Cloud Database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,14 +8,14 @@ const registerUser = async (req, res) => {
 
         res.status(201).json(
             {
-                sucesss: true,
+                success: true,
                 newUser,
                 message: "User Registration Sucessful"
             });
     } catch (err) {
         console.error("Error Registering User: ", err);
         res.status(500).json(
-            { message: err.message, sucesss: false });
+            { message: err.message, success: false });
     }
 }
 
@@ -26,12 +26,12 @@ const findUserByEmail = async (req, res) => {
         const user = await userService.findUserByEmail(req.body);
 
         res.status(200).json({
-            sucesss: true,
+            success: true,
             message: "Found User"
         })
     } catch (err) {
         res.status(404).json(
-            { message: err.message, sucesss: false });
+            { message: err.message, success: false });
     }
 }
 
@@ -60,7 +60,7 @@ const userLogin = async (req, res) => {
     } catch (err) {
         console.log("User Login Failed");
         res.status(500).json({
-            sucesss: false,
+            success: false,
             Error: err.message
         });
     };
--- src/services/cloudinaryConfig.js
+++ src/services/cloudinaryConfig.js
@@ -1,14 +1,14 @@
-import CloudinaryStorage from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = CloudinaryStorage({
+const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
         // Determine folder based on request URL
@@ -17,7 +17,7 @@ const storage = CloudinaryStorage({
         else if (req.originalUrl.includes("meal")) folderName = "meals";
         else if (req.originalUrl.includes("user")) folderName = "profile_pics";
         else if (req.originalUrl.includes("review")) folderName = "reviews";
-        
+
         return {
             folder: folderName,
             allowed_formats: ["jpg", "jpeg", "png"],
