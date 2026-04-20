9fb367ff0565c7b4cfbd0510397bd283f7675990
IT24100447 <it24100447@my.sliit.lk>
2026-04-20 12:46:35 +0530

    Added Logs for Cloudinary for debugging

--- src/services/cloudinaryConfig.js
+++ src/services/cloudinaryConfig.js
@@ -11,12 +11,16 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
+        console.log(`Cloudinary Starting upload for file: ${file.originalname}`);
+
         // Determine folder based on request URL
         let folderName = "others";
         if (req.originalUrl.includes("meal-request")) folderName = "meal_requests";
         else if (req.originalUrl.includes("meal")) folderName = "meals";
         else if (req.originalUrl.includes("user")) folderName = "profile_pics";
         else if (req.originalUrl.includes("review")) folderName = "reviews";
+
+        console.log(`Cloudinary Target folder: ${folderName}`);
 
         return {
             folder: folderName,
