2a74fbb00a0cf962dd338bb8c2c7fc881a64ce02
IT24100447 <it24100447@my.sliit.lk>
2026-04-20 13:05:08 +0530

    Updated Cloduinary Versions to match

--- src/services/cloudinaryConfig.js
+++ src/services/cloudinaryConfig.js
@@ -1,4 +1,4 @@
-import CloudinaryStorage from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 import multer from "multer";
 
